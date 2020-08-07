8545a910a19914259d7dda0d17a98e4323a4cbb1
Lucas Fabiano <lucasguaru@yahoo.com.br>
2020-08-06 22:22:11 -0300

    fazendo watch

--- app.js
+++ app.js
@@ -3,5 +3,8 @@ var primoApp = wag.module('primoApp', []);
 primoApp.controller('primoController', function($scope) {
 // primoApp.controller('primoController', function($scope, $http) {
     let vm = this;
-    $scope.nome = 'Wagner1';
+    $scope.nome = 'Wagner0000';
+    setTimeout(function() {
+        $scope.nome = 'Wagner1';
+    }, 100);
 });
--- wag.js
+++ wag.js
@@ -18,33 +18,57 @@ var WagFramework = function() {
 }
 
 var wag = new WagFramework();
+var handler = {
+    get: function(target, prop) {
+        return prop in target ?
+            target[prop] :
+            'Lucas';
+    },
+    set: function(target, prop, value) {
+        target[prop] = value;
+        render();
+    }
+};
+
+var scopeProxy = new Proxy({}, handler);
+
 let objetos = {
-    '$scope': {},
+    '$scope': scopeProxy,
     '$http': {}
 }
 
 window.addEventListener('load', function() {
+    render(true);
+});
+
+let rendering = false;
+function render(callController) {
     //DOM
+    if (rendering) {
+        return;
+    }
+    rendering = true;
     let wgAppDOM = document.querySelector('[wg-app]');
     let ctrlDOM = wgAppDOM.querySelector('[wg-controller]');
     let modelsDOM = ctrlDOM.querySelectorAll('[wg-model]');
     let ctrlName = ctrlDOM.getAttribute('wg-controller');
 
+    let scope = objetos['$scope'];
     //get controller e params
-    let fnCtrl = wag.controllers[ctrlName];
-    let fnCtrlStr = fnCtrl.toString();
-    let params = fnCtrlStr.substring(fnCtrlStr.indexOf('(') + 1, fnCtrlStr.indexOf(')')).replace(' ', '').split(',');
-    let arrayParam = [];
-    params.forEach(p => arrayParam.push(objetos[p]));
-    // console.log(params, arrayParam);
-
-    //invocar a funcao passando os params
-    let scope = objetos['$scope']
-    fnCtrl.apply(scope, arrayParam);
-    console.log(scope);
+    if (callController) {
+        let fnCtrl = wag.controllers[ctrlName];
+        let fnCtrlStr = fnCtrl.toString();
+        let params = fnCtrlStr.substring(fnCtrlStr.indexOf('(') + 1, fnCtrlStr.indexOf(')')).replace(' ', '').split(',');
+        let arrayParam = [];
+        params.forEach(p => arrayParam.push(objetos[p]));
+    
+        //invocar a funcao passando os params
+        fnCtrl.apply(scope, arrayParam);
+    }
 
     modelsDOM.forEach(model => {
         let modelField = model.getAttribute('wg-model');
         model.value = scope[modelField];
     })
-});
+    rendering = false;
+}
